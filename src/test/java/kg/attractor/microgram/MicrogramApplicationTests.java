6c1646a4bfcfa4ccddb444c610c8f2c032cd4b72
Erjan <tasmatoverzan05@gmail.com>
2025-05-16 22:29:42 +0600

    Первый коммит

---
+++ src/test/java/kg/attractor/microgram/MicrogramApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.microgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
